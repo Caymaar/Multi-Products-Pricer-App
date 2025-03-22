553ca8717aa408c2afc91be83a190a84c5b9fe75
aremiat <al.rem@outlook.fr>
2025-03-22 16:09:44 +0100

    add taux and stochastic processes

---
+++ Taux/abstract_taux.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractTaux(ABC):
+    @abstractmethod
+    def get_taux(self, x_val):
+        """
+        Retourne le taux interpolé pour une valeur donnée x_val.
+
+        :param x_val: float ou array-like, la ou les valeurs pour lesquelles interpoler le taux
+        :return: le taux interpolé
+        """
+        pass
+
+class AbstractYieldCurve(ABC):
+    @abstractmethod
+    def yield_curve(self, tau):
+        """
+        Calcule le taux pour une échéance donnée tau.
+
+        :param tau: float, l'échéance pour laquelle on souhaite calculer le taux
+        :return: le taux pour l'échéance donnée
+        """
+        pass
+
+    @abstractmethod
+    def yield_curve_array(self, taus):
+        """
+        Calcule la courbe des taux pour un tableau d'échéances.
+
+        :param taus: array-like, les échéances pour lesquelles on souhaite calculer les taux
+        :return: array-like, les taux correspondants
+        """
+        pass
+
+    @abstractmethod
+    def calibrate(self, taus, observed_yields, initial_guess):
+        """
+        Calibre les paramètres du modèle en minimisant l'erreur quadratique entre
+        les rendements observés et ceux générés par le modèle.
+
+        :param taus: array-like, les échéances
+        :param observed_yields: array-like, les rendements observés
+        :param initial_guess: array-like, estimation initiale des paramètres
+        :return: array-like, les paramètres calibrés
+        """
+        pass
---
+++ Taux/cubic.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.interpolate import interp1d
+from abstract_taux import AbstractTaux
+
+
+class CubicTaux(AbstractTaux):
+    def __init__(self, x, y, kind='cubic'):
+        """
+        Initialise l'interpolateur cubique.
+
+        Paramètres:
+        - x : array-like, les valeurs de référence (par exemple, temps ou indices)
+        - y : array-like, les taux correspondants à chaque valeur de x
+        - kind : type d'interpolation (par défaut 'cubic')
+        """
+        if len(x) != len(y):
+            raise ValueError("Les tableaux x et y doivent avoir la même longueur")
+
+        self.x = np.array(x)
+        self.y = np.array(y)
+        self.interpolateur = interp1d(self.x, self.y, kind=kind, fill_value="extrapolate")
+
+    def get_taux(self, x_val):
+        """
+        Retourne le taux interpolé pour une valeur donnée x_val.
+        """
+        return self.interpolateur(x_val)
